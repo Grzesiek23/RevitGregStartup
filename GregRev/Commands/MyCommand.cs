3c4796ca87e246a9f43c311558a9f8ba39dee7b4
Grzesiek <g.pasko@gmail.com>
2022-07-27 15:43:29 +0200

    Github init

---
+++ GregRev/Commands/MyCommand.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace GregRev.Commands
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class MyCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Welcome Greg!", "Hello world!");
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ GregRev/Init.cs
@@ -0,0 +1,46 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using System.Reflection;
+using GregRev.Commands;
+
+namespace GregRev
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Init : IExternalApplication
+    {
+        private const string TabName = "Greg's Tab";
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            PrepareRibbon(application);
+
+            return Result.Succeeded;
+
+        }
+
+        private void PrepareRibbon(UIControlledApplication application)
+        {
+
+            application.CreateRibbonTab(TabName);
+
+            var myPanel = application.CreateRibbonPanel(TabName, "Greg's panel");
+
+            var myButton = new PushButtonData(
+               "myButton",
+               "My first button",
+               Assembly.GetExecutingAssembly().Location, 
+               typeof(MyCommand).ToString())
+            {
+                ToolTip = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            };
+
+            myPanel.AddItem(myButton);
+        }
+    }
+}
